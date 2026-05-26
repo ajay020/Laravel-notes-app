bd1e0fe8862854f0b2437e1af1aefab440d54730
ajay <ajayjarwal44@gmail.com>
2026-05-26 17:26:10 +0530

    refactor: remove dashboard route and template file

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,6 @@ Route::middleware('auth')->group(function () {
     // Route::delete('/notes/{note}', [NoteController::class, 'destroy']);
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
